34e55ad2953842b75a3658b1de2be5bc89af8ebe
xAzzam0 <42500028+xAzzam0@users.noreply.github.com>
2018-08-25 22:16:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,24 +23,6 @@ const client = new Discord.Client({disableEveryone: true});
 const prefix = "1";
 
 
-client.on('ready', function(){    
-    var ms = 40000 ;    
-    var setGame = ['1Play'];    
-    var i = -1;    
-    var j = 0;    
-    setInterval(function (){    
-        if( i == -1 ){    
-j = 1;    
-       }    
-        if( i == (setGame.length)-1 ){    
-            j = -1;    
-      }    
-       i = i+j;    
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
-}, ms);    
-    
-});
-
 
 ////
 ///
